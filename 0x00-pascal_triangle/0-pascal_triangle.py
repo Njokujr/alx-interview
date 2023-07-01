1ebd61e1d8f49e38f9395894c470430630a0989e
njokujr <njokuvictory216@gmail.com>
2023-07-01 15:23:52 -0700

    correction to interview question

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,31 +3,30 @@
 pascal triangle
 """
 
+
 def pascal_triangle(n):
-	"""
-	Generate Pascal's triangle up to the given number of rows.
-	
-	Args:
-        	n (int): The number of rows in Pascal's triangle.
+    """
+    Generate Pascal's triangle up to the given number of rows.
+    Args:
+        n (int): The number of rows in Pascal's triangle.
+    Returns:
+        list: A list of lists representing Pascal's triangle.
 
-    	Returns:
-        	list: A list of lists representing Pascal's triangle.
+    Raises:
+        ValueError: If n is less than or equal to 0. or None.
+    """
+    if n <= 0:
+        return []
 
-    	Raises:
-        	ValueError: If n is less than or equal to 0. or None.
-	"""
-	if n <= 0:
-		return []
+    triangle = [[1]]
+    while len(triangle) < n:
+        prev_row = triangle[-1]
+        new_row = [1]
 
-	triangle = [[1]]
-	while len(triangle) < n:
-		prev_row = triangle[-1]
-		new_row = [1]
+        for i in range(len(prev_row) - 1):
+            new_row.append(prev_row[i] + prev_row[i + 1])
 
-		for i in range(len(prev_row) - 1):
-			new_row.append(prev_row[i] + prev_row[i + 1])
+        new_row.append(1)
+        triangle.append(new_row)
 
-		new_row.append(1)
-		triangle.append(new_row)
-
-	return triangle
+    return triangle
